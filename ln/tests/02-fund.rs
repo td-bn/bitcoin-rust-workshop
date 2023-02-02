ce171916ea72f34ff2f23fa2c50622d2a6afd073
bnabi <bnabi@protonmail.com>
2023-02-02 11:40:13 +0530

    add 02: fund channel

--- ln/tests/02-fund.rs
+++ ln/tests/02-fund.rs
@@ -4,7 +4,9 @@ use clightningrpc::LightningRPC;
 fn main() {
     let node = "ln-nodes/node-1/regtest/lightning-rpc";
     let client = LightningRPC::get_client(node);
-    let info = client.getinfo();
-    assert!(info.is_ok());
+    client.add_funds();
+    let funds_list = client.listfunds();
+    assert!(funds_list.is_ok());
+    assert!(funds_list.unwrap().outputs.len() > 0);
 }
 
